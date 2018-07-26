98495d1f4f8b743624716310ebea1b09481cc922
NidasioAlberto <nidasioalberto@gmail.com>
2018-07-26 13:05:23 +0100

    Improved the algoritm for match the header with the attributes, added an optional parameter to limit the number of header to check (from the fist)

--- Example.py
+++ Example.py
@@ -5,25 +5,27 @@ from xlsx_converter_to_django_class import convertXlsxToDjangoClass
 
 #define the class we want to use
 class DjangoClass:
-    clientId = ""
-    reportNo = ""
-    clientContact = ""
-    sampleRef = ""
-    dateReceived = ""
-    sampleID = ""
-    sampleReportNumber = ""
-    clientReference = ""
     alluminium = ""
+    clientId = ""
+    clientContact = ""
+    clientReference = ""
+    dateReceived = ""
+    reportNo = ""
+    nitrate = ""
+    nitrite = ""
+    sampleRef = ""
+    sampleReportNumber = ""
+    sampleID = ""
     totalColiforms = ""
 
 #define the path for the file to read
-filePathExample = "./Sample-data-2.xlsx"
-
+filePathExample = "./Sample-data-3.xlsx"
+ 
 #get the data with the library
-parsedData = convertXlsxToDjangoClass(DjangoClass, filePathExample)
+parsedData = convertXlsxToDjangoClass(DjangoClass, filePathExample, headerLimit=30)
 
 #show the results
 for elem in parsedData:
         print("element:")
         for header in [b[0] for b in [a for a in inspect.getmembers(DjangoClass, lambda a:not(inspect.isroutine(a))) if not(a[0].startswith('__') and a[0].endswith('__'))]]:
-            print("{0}: {1}".format(header, getattr(elem, header)))
+            print("\t{0}: {1}".format(header, getattr(elem, header)))
--- xlsx_converter_to_django_class.py
+++ xlsx_converter_to_django_class.py
@@ -3,99 +3,97 @@ from collections import namedtuple
 from fuzzywuzzy import fuzz
 import inspect
 
-def convertXlsxToDjangoClass(GivenClass, filePath, sheetName="Foglio1"):
-    wb = load_workbook(filename=filePath, read_only=True)
+def takeSecond(elem):
+    return elem[1]
 
-    #print(str(wb.sheetnames))
-
-    ws = wb[sheetName]
-
-    #get expected headers from the class
-
-    expectedHeaders = [b[0] for b in [a for a in inspect.getmembers(GivenClass, lambda a:not(inspect.isroutine(a))) if not(a[0].startswith('__') and a[0].endswith('__'))]]
-    #print(str(expectedHeaders))
-
-    #headers and table data
+def convertXlsxToDjangoClass(GivenClass, filePath, sheetNo=0, headerLimit=-1):
+    #0: initializing variables
     headers = []
     data = []
+    evaluetedHeaders = [] #[expected][actual]
+    sortedEvaluatedHeaders = [] #[expected][actual](index, header score)
+    headerPairs = [] #[(expected header index, actual header index), ...]
+    pairedData = [] #[(expected header, value), ...]
 
-    #grab the data and the headers
+    #1: get expected headers from the class
+    expectedHeaders = [b[0] for b in [a for a in inspect.getmembers(GivenClass, lambda a:not(inspect.isroutine(a))) if not(a[0].startswith('__') and a[0].endswith('__'))]]
+
+    #2: load the worksheet
+    wb = load_workbook(filename=filePath, read_only=True)
+    ws = wb[wb.sheetnames[sheetNo]]
+    
+    #3: grab the data and the headers from the worksheet
     for row in ws.rows:
         if len(headers) == 0:
             for cell in row:
-                headers.append(cell.value)
+                if headerLimit < 1:
+                    headers.append(cell.value)
+                elif headerLimit > len(headers):
+                    headers.append(cell.value)
         else:
             tmp = []
             for cell in row:
                 tmp.append(cell.value)
             data.append(tmp)
-
-    #display the data
-    #print("Data: ")
-    #print(len(data))
-
-    #print("headers")
-    #for header in headers:
-    #    print(header)
-
-    #print("data")
-    #for header in headers:
-    #    print(header)
-    #    for row in data:
-    #        print("\t{0}".format(row[headers.index(header)]))
-
-    #evaluate the headers
-    evaluetedHeaders = []
-    headersPairs = [] #(found, expected)
-
-    for expectedHeader in expectedHeaders:
+    
+    #4: evaluate the headers
+    for expectedHeader in expectedHeaders: #for each expeced header
         tmp = []
-        for header in headers:
-            tmp.append((header, fuzz.ratio(expectedHeader, header)))
+        for header in headers: #compare each actual header with the fuzzywuzzy lybrary
+            tmp.append(fuzz.ratio(expectedHeader, header))
         evaluetedHeaders.append(tmp)
 
-    for evaluation in evaluetedHeaders:
-        #print(evaluation)
+    #5: organise the cores
+    for i in range(0, len(expectedHeaders)):
+        scores = [] #(index, header score)
+        for j in range(0, len(headers)):
+            scores.append((j, evaluetedHeaders[i][j]))    
+        scores.sort(key=takeSecond)
+        #print(str(scores))
+        sortedEvaluatedHeaders.append(scores)
 
-        maxElem = evaluation[0]
-        for elem in evaluation:
-            if maxElem[1] < elem[1]:
-                maxElem = elem
+    # for i in range(0, len(headers)):
+    #     print("{0}: \t{1}".format(i, headers[i]))
 
-        #print(str(maxElem))
+    # print("evaluated headers")
+    # for i in range(0, len(evaluetedHeaders)):
+    #     print("{0}: \t{1}".format(expectedHeaders[i], str(evaluetedHeaders[i])))
 
-        #once the better one has been found delete if from all the other evaluations
-        for evaluation2 in evaluetedHeaders:
-            for header in evaluation2:
-                if header[0] == maxElem[0]:
-                    evaluetedHeaders[evaluetedHeaders.index(evaluation2)].pop(evaluation2.index(header))
+    # print("sorted evaluated headers")
+    # for i in range(0, len(sortedEvaluatedHeaders)):
+    #     print("{0}: \t{1}".format(expectedHeaders[i], str(sortedEvaluatedHeaders[i])))
 
-        #print(maxElem[0])
+    #6: choose the best header for each of the expected
+    for i in range(0, len(evaluetedHeaders)):
+        for j in range(0, len(headers)):
+            k = len(headers) - j - 1#reverse the counter to start from the highest score
+            bigger = True
+            for l in range(0, len(evaluetedHeaders)):
+                #current vs cicle
+                if evaluetedHeaders[i][sortedEvaluatedHeaders[i][k][0]] < evaluetedHeaders[l][sortedEvaluatedHeaders[i][k][0]]:
+                    bigger = False
+            if bigger:
+                # print("element found for {0}: {1}".format(expectedHeaders[i], headers[sortedEvaluatedHeaders[i][k][0]]))
+                headerPairs.append((i, sortedEvaluatedHeaders[i][k][0])) #(expected header index, actual header index)
+                break
+            # else:
+            #     print("bad position: [{0}][{1}] i:{2} k:{3} j:{4}".format(i, sortedEvaluatedHeaders[i][k][0], i, k, j))
 
-        headersPairs.append((maxElem[0], expectedHeaders[evaluetedHeaders.index(evaluation)]))
+    # print(str(headerPairs))
 
-    #show the headers once evalueted
-    #print(str(headersPairs))
-    #print(str(headers))
-
-    #pair headers with data
-    pairedData = []
-
+    #7: pair the expected headers with the data
     for row in data:
         tmp = []
-
-        if row:
-            for header in headersPairs:
-                #print(str(header))
-                tmp.append((header[1], row[headers.index(header[0])]))
-
+        if row: #check if the row has data in it
+            for headerPair in headerPairs:
+                tmp1 = expectedHeaders[headerPair[0]]
+                tmp3 = headerPair[1]
+                tmp2 = row[tmp3]
+                tmp.append((tmp1, tmp2))
+            # print(str(tmp))
             pairedData.append(tmp)
 
-    #show final paired data
-    #for row in pairedData:
-    #    print(str(row))
-
-    #put the data into the class
+    #8: populate the array of the given class with the data
     toReturn = []
     for row in pairedData:
         tmp = GivenClass()
@@ -104,9 +102,9 @@ def convertXlsxToDjangoClass(GivenClass, filePath, sheetName="Foglio1"):
         toReturn.append(tmp)
 
     #display the array of classes
-#    for elem in toReturn:
-#        print("element:")
-#        for header in expectedHeaders:
-#            print("{0}: {1}".format(header, getattr(elem, header)))
+    # for elem in toReturn:
+    #    print("element:")
+    #    for header in expectedHeaders:
+    #        print("{0}: {1}".format(header, getattr(elem, header)))
 
     return toReturn
